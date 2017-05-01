54ba2930e11dc52bb904210f8888bb06c3ca356e
Samuel Pastva <daemontus@gmail.com>
2017-05-01 17:01:08 +0200

    simplify toString

--- src/main/kotlin/com/github/sybila/huctl/Expression.kt
+++ src/main/kotlin/com/github/sybila/huctl/Expression.kt
@@ -1,6 +1,8 @@
 package com.github.sybila.huctl
 
-sealed class Expression {
+sealed class Expression(
+        private val string: String
+) {
 
     interface Binary<T> where T : Expression, T : Binary<T> {
         val left: Expression
@@ -8,36 +10,26 @@ sealed class Expression {
         fun copy(left: Expression = this.left, right: Expression = this.right): T
     }
 
-    data class Variable(val name: String) : Expression() {
-        override fun toString(): String = name
-    }
+    data class Variable(val name: String) : Expression(name)
 
-    data class Constant(val value: Double) : Expression() {
-        override fun toString(): String = String.format("%.4f", this.value) //no scientific notation
-    }
+    data class Constant(val value: Double) : Expression(String.format("%.6f", value))   // avoid scientific notation
 
     data class Add(
             override val left: Expression, override val right: Expression
-    ) : Expression(), Binary<Add> {
-        override fun toString(): String = "($left + $right)"
-    }
+    ) : Expression("($left + $right)"), Binary<Add>
 
     data class Sub(
             override val left: Expression, override val right: Expression
-    ) : Expression(), Binary<Add> {
-        override fun toString(): String = "($left - $right)"
-    }
+    ) : Expression("($left - $right)"), Binary<Add>
 
     data class Mul(
             override val left: Expression, override val right: Expression
-    ) : Expression(), Binary<Add> {
-        override fun toString(): String = "($left * $right)"
-    }
+    ) : Expression("$left * $right"), Binary<Add>
 
     data class Div(
             override val left: Expression, override val right: Expression
-    ) : Expression(), Binary<Add> {
-        override fun toString(): String = "($left / $right)"
-    }
+    ) : Expression("($left / $right)"), Binary<Add>
+
+    override fun toString(): String = string
 
 }
